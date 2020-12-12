1eb8ca14ef61c904ae646db26b02f272aeba366d
zeefola <zeefola@gmail.com>
2020-12-12 23:09:31 +0100

    Worked on the layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,7 +185,17 @@ $(function () {
     wow.init();
 
 
+    // Portfolio filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
 
+    $('#portfolio-filter li').on('click', function () {
+        $("#portfolio-filter li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
 
 
 
